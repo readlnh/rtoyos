5602a06f9a7c98dc0603de1aa399c76d2543590e
readlnh <readlnh@163.com>
2018-10-16 17:36:10 +0800

    string.h finished

---
+++ libs/string.c
@@ -0,0 +1,59 @@
+#include "string.h"
+#include "types.h"
+
+void memcpy(uint8_t *dest, const uint8_t *src, uint32_t len) 
+{
+    while(len--) {
+        *dest++ = *src++;
+    }
+}
+
+void memset(void *dest, uint8_t val, uint32_t len)
+{
+    uint8_t *dst = (uint8_t *)dest;
+    while(len --) {
+        *dst++ = val;
+    }
+}
+
+void bzero(void *dest, uint32_t len)
+{
+    memset(dest, 0, len);
+}
+
+int strcmp(const char *str1, const char *str2) 
+{
+    while(*str1 && *str2 && *str1 == *str2) {
+        str1++;
+        str2++;
+    }
+    return (*str1 - *str2); 
+}
+
+char *strcpy(char *dest, const char *src) 
+{
+    char *tmp = dest;
+    while(*src) {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+    return tmp;
+}
+
+char *strcat(char *dest, const char *src) {
+    char *cp = dest;
+    while(*cp) {
+        cp++;
+    }
+    
+    while((*cp++ = *src++));
+    
+    return dest;
+}
+
+int strlen(const char *src)
+{
+    const char *eos = src;
+    while(*eos++);
+    return (eos - src - 1);
+}
