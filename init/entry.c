877301466559b22bd023d5a85125a7081e9d0625
readlnh <readlnh@163.com>
2018-10-17 18:00:34 +0800

    printk finished

--- init/entry.c
+++ init/entry.c
@@ -1,8 +1,11 @@
 #include "console.h"
+#include "debug.h"
 
 int kernel_entry() 
 {
     console_clear();
-    console_write_color("Hello readlnh\n", rc_blue, rc_red);
+    //console_write_color("Hello readlnh\n", rc_blue, rc_red);
+    int x = 10;
+    printk("%d\n", x);
     return 0;
 }
